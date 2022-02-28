5a48840490ae08ff2615a39af83f750f6cde38b6
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-02-28 10:50:39 +0800

    Working hot

--- content-script.js
+++ content-script.js
@@ -1,14 +1,23 @@
 // This will be shown to an active tab
 let ashdevilen;
 chrome.runtime.onMessage.addListener((props, sender, response) => {
+    console.log(props)
     ashdevilen = props.ashdevilen
     AshDevilNow(ashdevilen)
 });
 
 window.onload = chrome.storage.sync.get(['ashdevilen'], function(obj) {
     AshDevilNow(obj.ashdevilen);
+    console.log("Working on ash devil in a nutshell")
 })
 
 function AshDevilNow(enabled) {
-
+    const selectedTags = document.querySelectorAll('*')
+    if (enabled) {
+        for (let i = 0; i < selectedTags.length; i++) {
+            if (selectedTags[i].innerHTML.includes('hot')) {
+                selectedTags[i].innerHTML = selectedTags[i].innerHTML.replace('hot', '🥵');
+            }
+        }
+    }
 }
